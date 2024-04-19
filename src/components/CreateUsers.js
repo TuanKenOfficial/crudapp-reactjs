22db8f55e2970718f11abd185a728cb831756fad
TuanKenOfficial <nhatyeuthu2013@gmail.com>
2024-04-19 14:33:13 -0700

    Update

--- src/components/CreateUsers.js
+++ src/components/CreateUsers.js
@@ -1,7 +1,54 @@
-export default function CreateUsers(){
-    return(
-        <h1>
-            Create Users
-        </h1>
-    )
+import { useState } from "react";
+export default function ListUsers() {
+  const [inputs, setInputs] = useState({});
+  const handleChage = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+    console.log(inputs);
+  };
+  return (
+    <div>
+      <h1>List Users</h1>
+      <form onSubmit={handleSubmit}>
+        <table cellSpacing={10}>
+          <tbody>
+            <tr>
+              <th>
+                <label>Name: </label>
+              </th>
+              <td>
+                <input type="text" name="name" onChange={handleChage} />
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label>Email: </label>
+              </th>
+              <td>
+                <input type="text" name="email" onChange={handleChage} />
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label>Mobile: </label>
+              </th>
+              <td>
+                <input type="text" name="mobile" onChange={handleChage} />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2} align="right">
+                <button>Save</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </div>
+  );
 }
